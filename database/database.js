b7c530397742b29e79e27a6d9f81224d7d6830a4
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2021-03-22 20:50:56 -0300

    added: timezone configuration

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const connection = new Sequelize('devblog', 'root', '1258', {
   host: '127.0.0.1',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: '-03:00'
 });
 
 
