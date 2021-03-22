37d68f28b613d07aeb39ac5d724773765fa67827
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2021-03-22 20:55:03 -0300

    home page route

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 // imports
-const express = require('express');
-const { request, response } = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { request, response } = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const connection = require('./database/database');
-const categoriesController = require('./categories/CategoriesController');
-const articlesController = require('./articles/ArticlesController');
-const Category = require('./categories/Category');
-const Article = require('./articles/Article');
+const connection = require("./database/database");
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+const Category = require("./categories/Category");
+const Article = require("./articles/Article");
 
 // app configs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // database
 connection
   .authenticate()
-  .then(() => console.log('🔌 Connected do Database.'))
+  .then(() => console.log("🔌 Connected do Database."))
   .catch((error) => console.log(error));
 
 // routes
 
-app.use('/', categoriesController);
+app.use("/", categoriesController);
 
-app.use('/', articlesController);
+app.use("/", articlesController);
 
-app.get('/', (request, response) => {
- response.render('index.ejs', 
- {
-   title: 'Home'
- });
+app.get("/", async (request, response) => {
+  Article.findAll({
+    include: [{ model: Category}]
+  }).then((articles) => {
+    response.render("index", {
+      title: "Home",
+      articles,
+    });
+  });
 });
 
-
 // port
-app.listen(3000, () => console.log('🔥 Back-end started! port: 3000'));
+app.listen(3000, () => console.log("🔥 Back-end started! port: 3000"));
